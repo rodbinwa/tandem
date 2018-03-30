358153e800a856b4f472ffba320574967081615b
rodbinwa <rodbray@live.com>
2018-03-29 20:30:14 -0700

    Moved log right

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,16 @@
 
-function myFunction() {
-    var x = document.getElementById("my-desktop-nav");
-    if (x.className === "desktop-nav") {
-        x.className += ".display-nav";
-    } else {
-        x.className = "desktop-nav";
-    }
-}
+// function myFunction() {
+//     var x = document.getElementById("my-desktop-nav");
+//     if (x.className === "desktop-nav") {
+//         x.className += ".display-nav";
+//     } else {
+//         x.className = "desktop-nav";
+//     }
+// }
+$(document).ready(function() {
+    $("#nav-hamburger").on("click", function(){
+        event.preventDefault();
+        $("#my-desktop-nav" ).toggleClass("display-nav");
+        // $(".desktop-nav").slideToggle();
+    });
+});
